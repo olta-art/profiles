503aca32ab708c44c2240d75c09d81505928a8ce
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-08-12 15:21:59 +0100

    sol/profile: add byte limits for each bit of data

--- contracts/Profiles.sol
+++ contracts/Profiles.sol
@@ -47,6 +47,11 @@ contract Profiles {
         external
         rateLimit
     {
+        require(bytes(profile.name).length         < 42,   "name too long");
+        require(bytes(profile.description).length  < 255,  "description too long");
+        require(bytes(profile.thumbnailURI).length < 2048, "thumbnail uri too long");
+        require(bytes(profile.linkURI).length      < 2048, "link uri too long");
+
         lastUpdated[msg.sender] = block.timestamp;
 
         emit updated(msg.sender, profile);
